a23de8eb519666b85ee3845e595ffc2a3597c24e
dexitional <ebenezerkb.ackah@gmail.com>
2023-06-16 15:39:01 +0000

    new updates checks

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
        <Banner />
        {/* @ts-ignore */}
        <UserBanner /> <AdminBanner />
-      {/* @ts-ignore */}
       <Statistics />     
        <BreakNews />
        <QuickGuide />
--- components/Statistics.tsx
+++ components/Statistics.tsx
@@ -5,16 +5,17 @@ import { GiVote } from 'react-icons/gi'
 import { BsPersonVcard } from 'react-icons/bs'
 import { fetchNominees } from '@/utils/serverApi'
 
-async function Statistics() {
+function Statistics() {
 
-  const nominations = await fetchNominees()
+ // const nominations = await fetchNominees()
 
   return (
     <div className="p-6 md:p-0 w-full bg-[#153B50]">
         <div className="mx-auto px-6 md:h-40 w-full md:max-w-6xl grid grid-cols-1 md:grid-cols-4 gap-y-8 md:gap-10 place-content-center">
             <StatPill title="VOTING CENTRES" value="10" Icon={FaVoteYea} />
             <StatPill title="ELECTORAL GROUPS" value="4" Icon={FaRegFlag} />
-            <StatPill title="FILLED NOMINATIONS" value={nominations?.total?.toString()} Icon={GiVote} />
+            {/* <StatPill title="FILLED NOMINATIONS" value={nominations?.total?.toString()} Icon={GiVote} /> */}
+            <StatPill title="FILLED NOMINATIONS" value="204" Icon={GiVote} />
             <StatPill title="ELIGIBLE VOTERS" value="33,000" Icon={BsPersonVcard} />
         </div>
     </div>

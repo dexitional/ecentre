b64d1d36ac33bdf84b93bfbdf72ea7c6253a40e3
dexitional <ebenezerkb.ackah@gmail.com>
2023-06-17 17:45:35 +0000

    new updates checks

--- app/(dashboard)/nominees/[id]/view/page.tsx
+++ app/(dashboard)/nominees/[id]/view/page.tsx
@@ -2,11 +2,11 @@ import FormPrintAdmin from '@/components/FormPrintAdmin'
 import Link from 'next/link'
 import React from 'react'
 
-function Page({ params }:{ params: { id: string }}) {
+function Page({ params }:{ params: { id: string, page: string }}) {
  
    return(
     <div className="space-y-6"> 
-      <Link href="/nominees" className="px-6 py-2 w-fit rounded border-2 border-blue-950 bg-slate-100 text-blue-950 font-extrabold print:hidden">GO BACK</Link>
+      <Link href={`/nominees?page=${params?.page || 1}`} className="px-6 py-2 w-fit rounded border-2 border-blue-950 bg-slate-100 text-blue-950 font-extrabold print:hidden">GO BACK</Link>
       <hr className="print:hidden mb-14"/>
       {/* @ts-ignore */}
       <FormPrintAdmin serial={params?.id} />

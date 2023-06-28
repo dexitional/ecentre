5ac0e359d603009dbcf9c090de6a91b0a8e1fa38
dexitional <ebenezerkb.ackah@gmail.com>
2023-06-28 18:50:52 +0000

    new

--- app/(applicant)/user/[serial]/smsfly/page.tsx
+++ app/(applicant)/user/[serial]/smsfly/page.tsx
@@ -12,24 +12,24 @@ export default async function Page({ params}:{ params: { serial: string }}) {
   //if(moment().isAfter(sess_res?.end_date)) redirect(`/user/${params?.serial}/printout`)
   // if(data[0]?.documents[0]?.form_submit && !moment().isAfter(sess_res?.end_date)) redirect(`/user/${params?.serial}/printout`)
   return (
-    <main className="px-6 py-4 flex-1 space-y-8">
+    <main className="p-2 md:px-6 md:py-4 flex-1 space-y-8">
          {/* Overview Cards  */}
-         <section className="grid grid-cols-4 gap-4 p-4">
-             <div className=" p-4 rounded border border-orange-300 bg-orange-50 flex flex-col justify-center space-y-2">
-                 <h1 className="px-4 py-2 rounded-lg bg-orange-400/90 font-bold text-center text-orange-100">SMS BALANCE</h1>
+         <section className="grid grid-cols-1 md:grid-cols-4 gap-4 p-2 md:p-4">
+             <div className=" p-4 rounded shadow-lg shadow-slate-600/30 bg-orange-50 flex flex-col justify-center space-y-2">
+                 <h1 className="px-4 py-2 rounded-lg bg-blue-950 font-bold text-center text-blue-100">SMS BALANCE</h1>
                  <p className="font-extrabold text-blue-950 text-2xl text-center italic">5000</p>
              </div>
-             <div className=" p-4 rounded border border-orange-300 bg-orange-50 flex flex-col justify-center space-y-2">
-                 <h1 className="px-4 py-2 rounded-lg bg-orange-400/90 font-bold text-center text-orange-100">CAMPAIGNS</h1>
+             <div className=" p-4 rounded shadow-lg shadow-slate-600/30 bg-orange-50 flex flex-col justify-center space-y-2">
+                 <h1 className="px-4 py-2 rounded-lg bg-blue-950 font-bold text-center text-blue-100">CAMPAIGNS</h1>
                  <p className="font-extrabold text-blue-950 text-2xl text-center italic">4</p>
              </div>
-             <div className=" p-4 rounded border border-orange-300 bg-orange-50 flex flex-col justify-center space-y-2">
-                 <h1 className="px-4 py-2 rounded-lg bg-orange-400/90 font-bold text-center text-orange-100">SENDER ID</h1>
+             <div className=" p-4 rounded shadow-lg shadow-slate-600/30 bg-orange-50 flex flex-col justify-center space-y-2">
+                 <h1 className="px-4 py-2 rounded-lg bg-blue-950 font-bold text-center text-blue-100">SENDER ID</h1>
                  <p className="font-extrabold text-blue-950 text-lg text-center italic">BRA-BARKOO</p>
              </div>
-             <div className=" p-4 rounded border border-orange-300 bg-orange-50 flex flex-col justify-center space-y-2">
-                <h1 className="px-4 py-2 rounded-lg bg-orange-400/90 font-bold text-center text-orange-100">BUY CREDIT</h1>
-                <div className="grid grid-cols-1">
+             <div className=" p-4 rounded shadow-lg shadow-slate-600/30 bg-orange-50 flex flex-col justify-center space-y-2">
+                 <h1 className="px-4 py-2 rounded-lg bg-blue-950 font-bold text-center text-blue-100">BUY CREDIT</h1>
+                 <div className="grid grid-cols-1">
                     <button className="py-0.5 px-1  border-4 border-black bg-yellow-400 text-black text-lg font-extrabold italic">MTN MoMo</button>
                     {/* <button className="py-0.5 px-1  border-4 border-pink-700 bg-blue-950 text-white text-xl font-extrabold italic">AirtelTigo</button> */}
                 </div>

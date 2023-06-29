d0eaa1b9f59be5a6a58d13f5a2c6e591eddc2519
dexitional <ebenezerkb.ackah@gmail.com>
2023-06-29 16:13:44 +0000

    new

--- app/(applicant)/user/[serial]/smsfly/page.tsx
+++ app/(applicant)/user/[serial]/smsfly/page.tsx
@@ -1,14 +1,36 @@
 
 "use client"
 import SMSCalculator from "@/components/SMSCalculator";
-import { useRef, useState } from "react";
+import { ChangeEvent, FormEvent, FormEventHandler, useRef, useState } from "react";
 
 
 export default async function Page({ params}:{ params: { serial: string }}) {
+  
+  const selRef = useRef<any>()
+  const [ form, setForm ] = useState<any>({})
+  const [ cost, setCost ] = useState<any>()
+  const onChange = (e: any) => {
 
-
-
+     const sms_count = +(selRef.current?.getAttribute('data-count'));
+     const cost = form.message?.length * sms_count;
+     setCost(cost)
+     setForm({ ...form, [e.target.name]: e.target.value })
+  }
  
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+      // Validations
+      const ok = window.confirm("SEND MESSAGE ?")
+      if(ok){
+         try {
+            // Validate - Cost with Credit balance
+            // Validate - SMS Text and allowed characters
+            // Send SMS
+         } catch (error) {
+           console.log(error)
+         }
+      }
+  }
+
   //if(moment().isAfter(sess_res?.end_date)) redirect(`/user/${params?.serial}/printout`)
   // if(data[0]?.documents[0]?.form_submit && !moment().isAfter(sess_res?.end_date)) redirect(`/user/${params?.serial}/printout`)
   return (
@@ -37,21 +59,25 @@ export default async function Page({ params}:{ params: { serial: string }}) {
          </section>
        
          <section className="flex">
-            <form className="p-4 flex-1">
-                 <textarea className="w-full h-48 rounded">Message is here!</textarea>
-                 <div className="grid grid-cols-2 gap-3">
-                    <div>
-                       <div></div>
-                       <button className="px-4 py-2 w-full bg-blue-950 text-white rounded">SEND MESSAGE</button>
-                    </div>
-                    <div>
-                        <h2>SMS COST</h2>
-                        <p>4332</p>
-                    </div>
-                 </div>
+            <form onSubmit={onSubmit} className="p-4 flex-1">
+                <textarea name="message" onChange={onChange} className="w-full max-h-48 rounded focus:ring-0 border border-slate-400 ">Message is here!</textarea>
+                <div className="grid grid-cols-2 gap-3">
+                  <div>
+                      <div>
+                          <label htmlFor="groupId"></label>
+                          <select name="groupId" onChange={onChange} ref={selRef} id="groupId">
+                            <option data-count="2000" value="">ADEHYE </option>
+                          </select>
+                      </div>
+                      <button className="px-4 py-2 w-full bg-blue-950 text-white rounded">SEND MESSAGE</button>
+                  </div>
+                  <div className="p-2 flex items-center space-x-4 border border-slate-400 bg-slate-50 text-blue-950">
+                      <h2 className="bg-blue-100 rounded p-1">SMS COST</h2>
+                      <p className="font-bold text-lg ">4332 {cost}</p>
+                  </div>
+                </div>
             </form>
             <SMSCalculator />
-            
          </section>
     </main>
   )

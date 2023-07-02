c27a7f980301c2d40aec740fc10f397492177bb6
Ebenezer Kwabena Blay Ackah <dexitional@Ebenezers-MacBook-Pro.local>
2023-07-02 19:36:36 +0000

    update forms

--- components/AspirantCardBox.tsx
+++ components/AspirantCardBox.tsx
@@ -11,21 +11,35 @@ type Props = {
 async function AspirantCardBox({ data }: Props) {
   
   const dm = await fetchRecord(data?.aspirant_regno)
-  const aspirant = dm?.data[0]?.user
-
+  const img = typeof data?.photo == 'string' ? data?.photo : Logo
+  const aspirant = dm?.success ? dm?.data[0]?.user : {};
+  //const aspirant = { name:''}
   return (
     <div className="relative pb-4 p-2 w-full flex items-center justify-between rounded-xl bg-blue-50 border-2 border-red-900 ">
         <div className="flex-1 flex flex-col items-center">
-            <Image src={data?.photo || Logo } alt="Candidate" className="p-1 object-cover object-top w-full h-64 rounded-lg bg-slate-50" height={100} width={100} />
+            <Image src={img} alt="Candidate" className="p-1 object-cover object-top w-full h-64 rounded-lg bg-slate-50" height={100} width={100} />
             <div className="space-y-1 flex-1 text-center">
                 <h2 className="px-2 text-base text-gray-800 font-bold tracking-wider uppercase">{aspirant?.name?.toUpperCase()}</h2>
                 <h2 className="px-2 py-1 bg-blue-200/60 rounded-2xl border-2 border-blue-950 font-extrabold text-blue-950 text-sm uppercase tracking-wider"><span className="italic">#</span> &nbsp;{data?.teaser}</h2>
             </div>
         </div>
         { true ?
-        <div className="absolute bottom-[30%] min-h-fit w-16 flex items-center rounded-md bg-white bg-opacity-80 border-2 border-blue-950 overflow-hidden">
-            { data?.ballot_no && <div className="p-2 text-2xl text-blue-950 font-bold">#</div>}
-            <div className="px-2 py-1 flex flex-wrap items-center justify-center rounded-tl rounded-bl bg-blue-950/95 italic text-2xl text-white font-bold tracking-widest">{data?.ballot_no || 'Q'}</div>
+        <div className="absolute bottom-[30%] min-h-fit w-fit flex items-center rounded-md bg-white bg-opacity-80 border-2 border-blue-950 overflow-hidden">
+            { data?.ballot_no 
+            ? <div className="p-2 text-2xl text-blue-950 font-bold">#</div>
+            ? data?.vetscore 
+            : <div className="p-2 text-lg text-blue-950 font-bold">{data?.vetscore}%</div> 
+            : <div className="p-2 text-lg text-blue-950 font-bold"></div> 
+            }
+            <div className="px-2 py-1 flex flex-wrap items-center justify-center rounded-tl rounded-bl bg-blue-950/95 italic text-2xl text-white font-bold tracking-widest">
+                {
+                  data?.ballot_no 
+                  ? data?.ballot_no
+                  : data?.is_candidate
+                  ? 'Q'
+                  : ''
+                }
+            </div>
         </div>
         : null
         }
--- components/PageDisplay.tsx
+++ components/PageDisplay.tsx
@@ -7,6 +7,8 @@ import { options } from '@/options'
 import { fetchNomineesDisplay, fetchPosition } from '@/utils/serverApi'
 import { getGroup } from '@/utils/getGroup'
 import AspirantCardBox from './AspirantCardBox'
+import DisplayHeader from './DisplayHeader'
+import Print from './Print'
 
 type Props = {}
 
@@ -38,18 +40,14 @@ async function PageDisplay({}: Props) {
 
   return (
     <div className="w-full">
-        <header className="relative h-24 bg-red-800 flex items-center rounded-md">
-            <h1 className="absolute top-4 left-[50%] -translate-x-[50%] text-5xl print:scale-95 text-white font-poppins font-bold tracking-widest ">{new Date().getFullYear()}/{new Date().getFullYear()+1}</h1>
-            <h2 className="z-20 px-6 py-3 w-4/5 h-14 flex items-center justify-center rounded-tl-full rounded-bl-full bg-blue-950 text-blue-50 text-3xl print:text-2xl font-bold tracking-widest absolute top-16 left-[50%] -translate-x-[50%] ">{group?.title?.toUpperCase()}</h2>
-            <h2 className="z-10 px-4 py-5 pb-0 w-3/5 h-14 flex items-center justify-center rounded-br-full rounded-bl-full border-4 border-blue-950 bg-blue-100 text-blue-950 text-2xl print:text-2xl font-extrabold tracking-widest absolute top-24 left-[50%] -translate-x-[50%] ">DISPLAY OF ASPIRANTS</h2>
-            <div className="z-20 p-2 absolute right-10 top-6 bg-white shadow-xl shadow-slate-900/30  rounded-full">
-               <div className="h-20 w-20 flex items-center justify-center "><Image src={Logo} className="w-16 object-cover rounded" alt="Logo" height={100} width={100} /></div>
-            </div>
-        </header>
-        <main className="mt-32 my-10 space-y-10">
+        <DisplayHeader title={group?.title} />
+        <main className="mt-24 print:mt-0 my-10 space-y-10">
+            <div className="mx-auto w-fit rounded text-lg print:hidden border-2 border-red-700/50 uppercase"><Print /></div>
+        
             { Object.entries(data).map( ([key, value]: any) => {
              return(
-                <section key={key} className="space-y-6 pb-10 border-b-2 border-red-700/80 border-dashed">
+                <section key={key} className="space-y-6 pb-10 print:border-0 border-b-2 border-red-700/80 border-dashed print:break-after-page">
+                    <DisplayHeader title={group?.title} print={true} />
                     <h1 className="px-6 py-1 mx-auto w-fit -skew-x-12 bg-red-50/30 border-8 border-double border-red-700/80 text-red-800 text-2xl text-center font-extrabold tracking-widest">{key?.toUpperCase()}</h1>
                     <div className="grid gap-4 justify-center grid-cols-[repeat(auto-fit,_32%)]">
                      { value?.map( (aspirant: any) => {
